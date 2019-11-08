dde4c5f05347a698f829b86965b8243f7a2051cc
powhound4 <george.hatch@fathym.com>
2019-11-07 18:26:30 -0700

    fix styling

--- projects/common/src/lib/lcu-progress-circle.module.ts
+++ projects/common/src/lib/lcu-progress-circle.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProgressCircleComponent } from './controls/progress-circle/progress-circle.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [ProgressCircleComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule
   ],
   exports: [ProgressCircleComponent],
   entryComponents: [ProgressCircleComponent]
